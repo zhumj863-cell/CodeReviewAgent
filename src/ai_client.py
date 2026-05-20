0794968282fb10ee6b3a424b1f58d5cfd1448135
Mingjuan Zhu <zmj@amazon.com>
2026-05-20 21:53:38 +0800

    Use langchain.

--- src/ai_client.py
+++ src/ai_client.py
@@ -1,6 +1,11 @@
-from openai import OpenAI
 from langchain_openai import ChatOpenAI
-openai_client = OpenAI(
-    api_key= "",
+
+from constant import OPENAI_MODEL
+from tools import TOOLS
+
+llm = ChatOpenAI(
+    model=OPENAI_MODEL,
+    api_key="",
     base_url="https://api.deepseek.com"
 )
+llm_with_tools = llm.bind_tools(TOOLS)
--- src/reviewer.py
+++ src/reviewer.py
@@ -1,9 +1,5 @@
-import json
-
-from ai_client import openai_client
-from constant import OPENAI_MODEL
+from src.ai_client import llm_with_tools
 from src.tools import count_lines, list_functions
-from tools import TOOLS
 
 TOOL_MAP = {
     "count_lines": count_lines,
@@ -17,27 +13,21 @@ def review(file_path):
         {"role": "user", "content": "帮我审查下面的代码，文件路径是 {}, 内容是 {}".format(file_path, file_content)}
     ]
     while True:
-        response = openai_client.chat.completions.create(
-            model=OPENAI_MODEL,
-            messages=messages,
-            tools=TOOLS,
-        )
-        message = response.choices[0].message
-        if message.tool_calls:
-            messages.append(message)
-            for tool_call in message.tool_calls:
-                func = TOOL_MAP.get(tool_call.function.name)
-                args = json.loads(tool_call.function.arguments)
-                result = func(**args) if func else 'None'
+        response = llm_with_tools.invoke(messages)
+        print(response)
+        if response.tool_calls:
+            messages.append(response)
+            for tool_call in response.tool_calls:
+                func = TOOL_MAP.get(tool_call["name"])
+                args = tool_call["args"]
+                result = func.invoke(args) if func else 'None'
                 messages.append({
                     "role": "tool",
-                    "tool_call_id": tool_call.id,
+                    "tool_call_id": tool_call["id"],
                     "content": str(result)
                 })
         else:
-            print(message.content)
             break
-
 
 def read_file(file_path):
     with open(file_path, encoding="utf-8") as file:
